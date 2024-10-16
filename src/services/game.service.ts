9c184d7d7013e2e795e21ba11f7c731cd7182dae
Yannis1112 <yannis.philippot@gmail.com>
2024-10-16 15:33:59 +0200

    Question 5.2

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,7 +1,9 @@
 import { GameDTO } from "../dto/game.dto";
+import { ReviewFound } from "../error/ExistingError";
 import { notFound, notFoundConsole } from "../error/NotFoundError";
 import { Console } from "../models/console.model";
 import { Game } from "../models/game.model";
+import { Review } from "../models/review.model";
 
 export class GameService {
   public async getAllGames(): Promise<GameDTO[]> {
@@ -39,15 +41,11 @@ export class GameService {
   public async deleteGame(id: number): Promise<void> {
     const game = await Game.findByPk(id);
     if (game) {
-
-      const games = await Game.findAll({ where: { console_id: id } });
-      for (const game of games) {
-        const reviews = await Review.findAll({ where: { game_id: game.id } });
-        if (reviews.length > 0) {
-          ReviewFound("Reviews");
-        }
+      const reviews = await Review.findAll({ where: { game_id: game.id } });
+      if (reviews.length > 0) {
+        ReviewFound("Reviews");
       }
-      await console.destroy();
+      await game?.destroy();
     } else {
       notFound(id.toString());
     }
